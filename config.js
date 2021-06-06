6124ceb09331abcbe3211638498a437eea732851
Gaurav Khulway <47207388+GauravKhulway@users.noreply.github.com>
2021-06-06 18:54:50 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+//Note: First make a developer account on twitter and get your API keys to start from here
+module.exports= {
+  consumer_key:         '',//API key
+  consumer_secret:      '',//API secret key
+  access_token:         '',//token 
+  access_token_secret:  '',//token secret
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+}
